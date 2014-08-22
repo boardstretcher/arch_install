96feee93e1b22b10c3aa148d520727ac949567ac
boardstretcher <boardstretcher@gmail.com>
2014-08-21 20:38:03 -0400

    Create 1st_part.sh

---
+++ 1st_part.sh
@@ -0,0 +1,40 @@
+# assuming wireless network
+	rfkill unblock 0
+	rfkill unblock 1
+	ip link set wlp18s0 down
+	wifi-menu wlp18s0
+
+# set time
+	ntpdate pool.ntp.org 
+	hwclock --systohc
+
+# update pacman and system
+	pacman-key --init
+	pacman-key --populate
+	pacman -Syu
+
+# partition disk (manually for now)
+
+	cfdisk /dev/sda
+		# new, primary, 20g, bootable
+		# new, primary, 1024m, type, swap
+		# new, primary, all the rest
+		# write, quit
+
+# install file system on disk, activate swap
+	mkfs.ext4 /dev/sda1
+	mkfs.ext4 /dev/sda3
+	mkswap /dev/sda2
+
+# mount file systems and swap
+	swapon /dev/sda2
+	mount /dev/sda1 /mnt
+	mkdir /mnt/home
+	mount /dev/sda3 /mnt/home
+
+# bootstrap the new filesystem
+	pacstrap /mnt base base-devel
+	genfstab -p /mnt > /mnt/etc/fstab
+
+# chroot to the new linux system	
+	arch-chroot /mnt
