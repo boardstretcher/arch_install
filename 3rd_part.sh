b39ec5e080e804c7e59556aeb785e06c3ea378d6
boardstretcher <boardstretcher@gmail.com>
2014-08-21 20:41:08 -0400

    Create 3rd_part.sh

---
+++ 3rd_part.sh
@@ -0,0 +1,23 @@
+# bring up lan, sync, add a nonroot user
+	ip link set wlp18s0 up
+	wifi-menu wlp18s0
+	pacman --sync --refresh --sysupgrade
+
+	useradd -G lp,games,video,audio,optical,storage,scanner,power,users sysop
+	passwd sysop
+
+# set system time
+	pacman -S ntp
+	ntpdate pool.ntp.org
+
+# Required packages (Window Manager, Sound, Wireless)
+	pacman -S alsa-utils xorg-server xorg-xinit xorg-server-utils xf86-video-intel \
+	xf86-input-synaptics xterm openbox obconf wget obmenu nitrogen tint2 rox dmenu \
+	ristretto volwheel conky xcompmgr rfkill iptables ntfs-3g util-linux \
+	acpid gnupg cups gutenprint perl-file-mimeinfo gnome-font-viewer scrot slim
+
+
+# extra/optional packages (to make a decent desktop)
+	pacman -S openbox-themes chromium firefox flashplugin xorg-xev tcpdump openssh \
+	rsync xpdf aria2 cups-pdf meld cpio tar gzip zip unzip unrar p7zip arj lha \
+	xz lzop w3m  
